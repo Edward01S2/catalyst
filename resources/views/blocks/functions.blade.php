356256ea9fabc3af5deb36b83494142e80e5615b
edward01s2 <edward.01s2@gmail.com>
2020-09-11 09:49:08 -0500

    Remove service link

--- resources/views/blocks/functions.blade.php
+++ resources/views/blocks/functions.blade.php
@@ -12,10 +12,10 @@
               <a class="inline-block transition transform duration-300 hover:-translate-y-px" href="{!! $func['link'] !!}">{!! $func['title'] !!}</a>
             </h3>
             <p class="lg:text-lg">{!! $func['excerpt'] !!}</p>
-            <a class="block flex items-center group transform transition duration-300 hover:translate-x-2" href="{!! $func['link'] !!}">
+            {{-- <a class="block flex items-center group transform transition duration-300 hover:translate-x-2" href="{!! $func['link'] !!}">
               <span class="mr-2 tracking-wider font-bold lg:text-lg">Learn more</span>
               <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
+            </a> --}}
           </div>
           <div class="overflow-hidden h-60 lg:h-72 xl:h-84">
             <img class="w-full object-contain" src="{!! $func['image'] !!}" alt="">
